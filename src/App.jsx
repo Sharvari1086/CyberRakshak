2f83659c7aea71c35aa8bd5bec70b90a8cd99884
Sharvari1086 <patilsharvari500@gmail.com>
2026-02-21 13:09:23 +0530

    dataset

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import CyberRakshakDashboard from './CyberRakshakDashboard';
+import Lawbot from './pages/Lawbot';
 
 
 export default function App() {
   return (
-    <CyberRakshakDashboard />
+    <Lawbot />
   );
 }
